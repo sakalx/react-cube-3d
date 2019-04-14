789dad6e05dad08e42f657a4a6c8cb77ab6b79d4
sakal <sakals@mail.ua>
2019-04-14 19:11:55 -0400

    fix: handle rest of props
    Signed-off-by: sakal <sakals@mail.ua>

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ function Cube({
                 children,
                 cubeSize = '300px',
                 viewportSize = '350px',
+                ...rest
               }) {
   const viewportEl = useRef(null);
   let isUpperSideDown = false;
@@ -22,9 +23,9 @@ function Cube({
   };
 
   useEffect(() => {
+    const viewport = viewportEl.current.style;
     const cubeSizeSplitted = cubeSize.split(/(\d+)/);
     const translateZ = (cubeSizeSplitted[1] / 2) + cubeSizeSplitted[2];
-    const viewport = viewportEl.current.style;
 
     viewport.setProperty('--cube-size', cubeSize);
     viewport.setProperty('--viewport-size', viewportSize);
@@ -63,39 +64,41 @@ function Cube({
           : x.prevPosition + distanceX;
       const nextY = y.prevPosition - distanceY;
 
-      //const isXChanged = nextX > x.current + 10 || nextX < x.current - 10;
-      //const isYChanged = nextY > y.current + 10 || nextY < y.current - 10;
+      const isXChanged = nextX > x.current + 10 || nextX < x.current - 10;
+      const isYChanged = nextY > y.current + 10 || nextY < y.current - 10;
 
-      //if (isXChanged || isYChanged) {
-      x.current = nextX;
-      y.current = nextY;
-      rotate(nextX, nextY);
-      //}
+      if (isXChanged || isYChanged) {
+        x.current = nextX;
+        y.current = nextY;
+        rotate(nextX, nextY);
+      }
     }
   };
 
   return (
-      <div
-          id='-cube-viewport'
-          onMouseDown={handleStartMoving}
-          onMouseMove={handleMove}
-          onMouseUp={handleStopMoving}
-          onMouseOut={handleStopMoving}
-          onTouchStart={e => handleStartMoving(e.changedTouches[0])}
-          onTouchMove={e => handleMove(e.changedTouches[0])}
-          onTouchEnd={handleStopMoving}
-          ref={viewportEl}
-      >
-        <div id='-cube-axis'>
-          {children.map((component, i) =>
-              <div
-                  className='-cube-side'
-                  id={`-cube-side-${i}`}
-                  key={String(i)}
-              >
-                {component}
-              </div>,
-          )}
+      <div {...rest}>
+        <div
+            id='-cube-viewport'
+            onMouseDown={handleStartMoving}
+            onMouseMove={handleMove}
+            onMouseUp={handleStopMoving}
+            onMouseOut={handleStopMoving}
+            onTouchStart={e => handleStartMoving(e.changedTouches[0])}
+            onTouchMove={e => handleMove(e.changedTouches[0])}
+            onTouchEnd={handleStopMoving}
+            ref={viewportEl}
+        >
+          <div id='-cube-axis'>
+            {children.map((component, i) =>
+                <div
+                    className='-cube-side'
+                    id={`-cube-side-${i}`}
+                    key={String(i)}
+                >
+                  {component}
+                </div>,
+            )}
+          </div>
         </div>
       </div>
   );
